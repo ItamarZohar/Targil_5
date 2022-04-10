c83592c25efe3ab4f951d389198fee19c6cc00f9
itamar zohar <etamarxlll3@gmail.com>
2022-04-10 16:55:22 +0300

    drisha 2

--- src/Data_Layer/DataSource.java
+++ src/Data_Layer/DataSource.java
@@ -2,11 +2,17 @@ package Data_Layer;
 
 
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.CharBuffer;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class DataSource {
@@ -16,7 +22,7 @@ public class DataSource {
     public static List<Product> allProducts;
     public static List<OrderProduct> allOrderProducts;
     // Update this path according to your data files location
-    public static String basePath = "";
+    public static String basePath = "C:\\Users\\itamar\\Desktop\\DataFiles\\";
     public static String customersPath = basePath +"customers.txt";
     public static String ordersPath = basePath +"orders.txt";
     public static String productsPath = basePath +"products.txt";
@@ -32,23 +38,49 @@ public class DataSource {
         } catch (IOException e) { e.printStackTrace(); }
     }
     public static List<Customer> readCustomersfromFile() throws IOException {
-        //To Do
-        return null;
+        try (Stream<String> stream = Files.lines(Path.of(customersPath)))
+        {
+          stream.forEach(e -> allCustomers.add(new Customer(e.toString())));
+        }catch (IOException E){E.printStackTrace();}
+
+        return allCustomers;
     }
 
     public static List<Order> readOrdersfromFile() throws IOException {
-        //To Do
-        return null;
+        try (Stream<String> stream = Files.lines(Path.of(ordersPath)))
+        {
+
+            stream.forEach(e -> {
+                try {
+                    allOrders.add(new Order(e.toString()));
+                } catch (ParseException ex) {
+                    ex.printStackTrace();
+                }
+            });
+
+        }catch (IOException E){E.printStackTrace();}
+
+
+        return allOrders;
     }
 
     public static List<Product> readProductsfromFile() throws IOException {
         //To Do
-        return null;
+        try (Stream<String> stream = Files.lines(Path.of(productsPath)))
+        {
+            stream.forEach(e -> allProducts.add(new Product(e.toString())));
+        }catch (IOException E){E.printStackTrace();}
+
+        return allProducts;
     }
 
     public static List<OrderProduct> readOrderProductsfromFile() throws IOException {
-        //To Do
-        return null;
+        try (Stream<String> stream = Files.lines(Path.of(orderProductPath)))
+        {
+            stream.forEach(e -> allOrderProducts.add(new OrderProduct(e.toString())));
+        }catch (IOException E){E.printStackTrace();}
+
+        return allOrderProducts;
     }
 }
 
